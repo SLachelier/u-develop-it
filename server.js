72e29959eb43549f13d8b7974688cfeb277afbd8
Shauna L <slachelier@gmail.com>
2022-07-25 21:24:11 -0500

    added queries

--- server.js
+++ server.js
@@ -1,31 +1,56 @@
 const mysql = require('mysql2');
-const express = require ('express');
+const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-//express middleware
+// Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//Default response for any other request (Not Found)
+// Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.app.status(404).end();
+    res.status(404).end();
 });
-
-//Connect to database
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        //Your MySQL username
-        user: 'SLachelier',
-        //Your MySQL password
-        password: '$H@LaN@Na7212',
-        database: 'election'
-    },
-    console.log('Connected to the election database.')
-);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+    host: 'localhost',
+      // Your MySQL username,
+    user: 'root',
+      // Your MySQL password
+    password: '$H@LaN@Na7212',
+    database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+//GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//     console.log(err);
+//     }
+//     console.log(row);
+// });
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+                            VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
